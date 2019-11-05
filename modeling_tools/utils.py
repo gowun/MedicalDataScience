2decc858a6170bf6435c708f71839e505345584c
Gowun Jeong <kennie@Gowunui-MacBook-Pro.local>
2019-11-05 13:53:25 +0900

    change a little

--- modeling_tools/utils.py
+++ modeling_tools/utils.py
@@ -1,16 +1,12 @@
 import pandas as pd 
 import numpy as np 
-import os
 import pickle
 
 def save_as_file_colab(obj, path, mode):
   if mode == 'csv':
-    nm = 'tmp.csv'
-    obj.to_csv(nm, index=False)
+    obj.to_csv(path, index=False)
   elif mode == 'pickle':
-    nm = 'tmp.pkl'
-    pickle.dump(obj, open(nm, 'wb'))
-  os.system('cp ' + nm + " '"+ path + "'")
+    pickle.dump(obj, open(path, 'wb'))
 
 
 def load_data(path, mode):
