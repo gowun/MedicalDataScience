57cc5c583d7672096329cd97d94e17ea545495be
Gowun Jeong <kennie@Gowunui-MacBook-Pro.local>
2019-11-13 13:43:25 +0900

    0 to 0.0

--- modeling_tools/preprocessing.py
+++ modeling_tools/preprocessing.py
@@ -67,7 +67,7 @@ def filter_by_VIF_MI(df, features, y, upper_limit=100):
     total = pd.DataFrame({'feature': vs, 'MI': mutual_info_classif(df[vs], y, n_neighbors=len(vs), random_state=1234)})
     total['VIF'] = list(map(lambda x: variance_inflation_factor(df[vs].values, x), range(len(vs))))
 
-    tmp = total.loc[total['MI'] > 0].loc[total['VIF'] <= upper_limit]
+    tmp = total.loc[total['MI'] > 0.0].loc[total['VIF'] <= upper_limit]
     if len(tmp) < 5:
         total['MI/VIF'] = total['MI'] / total['VIF']
         total = total.sort_values(by='MI/VIF', ascending=False).drop(['MI/VIF'], 1)[:5]
