6097ef00a69053362c9084ef94b492b365b88394
Gowun Jeong <kennie@Gowunui-MacBook-Pro.local>
2019-11-20 14:49:25 +0900

    minor revision

--- BladderCancer_Cho/BladderCancer_Cho.py
+++ BladderCancer_Cho/BladderCancer_Cho.py
@@ -13,8 +13,8 @@ from ..modeling_tools import utils as ut
 class BladderCander():
     def __init__(self):
         self.home_path = os.path.abspath('MedicalDataScience/BladderCancer_Cho/') + '/'
-        self.Xy = ut.load_data(self.home_path + 'X_y_event_durations.pkl', 'pickle')
-        self.event, self.durations = ut.load_data(self.home_path + 'event_durations.pkl', 'pickle')
+        X, y, self.event, self.durations = ut.load_data(self.home_path + 'X_y_event_durations.pkl', 'pickle')
+        self.Xy = [X, y]
         self.selected_Xs_with_importance = ut.load_data(self.home_path + 'selected_Xs.csv', 'csv')
         self.col_combinations = ut.load_data(self.home_path + 'col_combinations.pkl', 'pickle')
         self.only_marker_combinations = ut.load_data(self.home_path + 'only_marker_combinations.pkl', 'pickle')
