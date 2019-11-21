251bef0c84ee8bc7ce0b6f96a152204e997eacde
Gowun Jeong <kennie@Gowunui-MacBook-Pro.local>
2019-11-21 22:45:56 +0900

    upload figure_eda

--- modeling_tools/figure_eda.py
+++ modeling_tools/figure_eda.py
@@ -1,27 +1,50 @@
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+#import plotly.figure_factory as ff
+
+
+def divide_data(arr1d, data_labels=None):
+    if data_labels is not None:
+        labels = sorted(set(data_labels))
+        data_arr = []
+        for l in labels:
+            data_arr.append(np.array(arr1d)[np.array(data_labels) == l])
+    else:
+        labels = None
+        data_arr = [np.array(arr1d)]
+    return data_arr, labels
 
 
 ## mode in ['overlay', 'stack']
 def plot_histogram(arr1d, mode, data_labels=None, xaxis=None, title=None):
-  fig = go.Figure()
-  if data_labels is not None:
-    labels = sorted(set(data_labels))
-    data_arr = []
-    for l in labels:
-      data_arr.append(np.array(arr1d)[np.array(data_labels) == l])
-  else:
-    data_arr = [np.array(arr1d)]
-  for i, d in enumerate(data_arr):
-    if data_labels is not None:
-      l = labels[i]
-    else:
-      l = None
-    fig.add_trace(go.Histogram(x=d, name=l))
+    data_arr, labels = divide_data(arr1d, data_labels) 
+    fig = go.Figure()
+    for i, d in enumerate(data_arr):
+        if labels is not None:
+            l = labels[i]
+        else:
+            l = None
+        fig.add_trace(go.Histogram(x=d, name=l, bin_size=.2))
 
-  arg_dict = {'yaxis_title_text': 'Count', 'barmode': mode, 'xaxis_title_text': xaxis, 'title_text': title}
-  fig.update_layout(**arg_dict)
-  if mode == 'overlay':
-    fig.update_traces(opacity=0.75)
-  fig.show()
+    arg_dict = {'yaxis_title_text': 'Count', 'barmode': mode, 'xaxis_title_text': xaxis, 'title_text': title}
+    fig.update_layout(**arg_dict)
+    if mode == 'overlay':
+        fig.update_traces(opacity=0.75)
+    fig.show()
+
+
+def plot_box(arr1d, axis, data_labels, axis_title):
+    data_arr, labels = divide_data(arr1d, data_labels)
+    fig = go.Figure()
+    for i, l in enumerate(labels):
+        if axis == 'x':
+            fig.add_trace(go.Box(x=data_arr[i], name=l))
+        elif axis == 'y':
+            fig.add_trace(go.Box(y=data_arr[i], name=l))
+    
+    if axis == 'x':
+        fig.update_layout(xaxis_title=axis_title)
+    elif axis == 'y':
+        fig.update_layout(yaxis_title=axis_title)
+    fig.show()
